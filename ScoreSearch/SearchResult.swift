b1b94fd6c8fd30a2434cdd4e42cf8433d94930c6
akula <akulinind@gmail.com>
2022-07-11 17:38:52 +0300

    feat: add simple searchbar layout

---
+++ ScoreSearch/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  ScoreSearch
+//
+//  Created by Dmytro Akulinin on 11.07.2022.
+//
+
+import Foundation
+
+class SearchResult {
+  var name = ""
+  var artistName = ""
+}
