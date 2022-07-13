a71fe7172efb203ec7a58894ffb0721805a4fb59
akula <akulinind@gmail.com>
2022-07-13 14:35:23 +0300

    feat: implement fetching results from iTunes

--- ScoreSearch/SearchResultCell.swift
+++ ScoreSearch/SearchResultCell.swift
@@ -8,18 +8,19 @@
 import UIKit
 
 class SearchResultCell: UITableViewCell {
-  @IBOutlet weak var nameLabel: UILabel!
-  @IBOutlet weak var artistNameLabel: UILabel!
-  @IBOutlet weak var artworkImageView: UIImageView!
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+  @IBOutlet var nameLabel: UILabel!
+  @IBOutlet var artistNameLabel: UILabel!
+  @IBOutlet var artworkImageView: UIImageView!
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    let selectedView = UIView(frame: CGRect.zero)
+    selectedView.backgroundColor = UIColor(named: "SearchBar")?.withAlphaComponent(0.5)
+    selectedBackgroundView = selectedView
+  }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
-    }
-
+    // Configure the view for the selected state
+  }
 }
