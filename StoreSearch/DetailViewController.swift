2554926109cd74ed77a6a04c486968aeb95fa46e
akula <akulinind@gmail.com>
2022-07-18 12:20:35 +0300

    feat: add animation and gradient background for pop-up

--- StoreSearch/DetailViewController.swift
+++ StoreSearch/DetailViewController.swift
@@ -41,6 +41,16 @@ class DetailViewController: UIViewController {
     if searchResult != nil {
       updateUI()
     }
+    // Gradient view
+    view.backgroundColor = UIColor.clear
+    let dimmingView = GradientView(frame: CGRect.zero)
+    dimmingView.frame = view.bounds
+    view.insertSubview(dimmingView, at: 0)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    transitioningDelegate = self
   }
   
   // MARK: - Helper Methods
@@ -99,5 +109,21 @@ class DetailViewController: UIViewController {
 extension DetailViewController: UIGestureRecognizerDelegate {
   func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
     return (touch.view === self.view)
+  }
+}
+
+extension DetailViewController: UIViewControllerTransitioningDelegate {
+  func animationController(
+    forPresented presented: UIViewController,
+    presenting: UIViewController,
+    source: UIViewController
+  ) -> UIViewControllerAnimatedTransitioning? {
+    return BounceAnimationController()
+  }
+  
+  func animationController(
+    forDismissed dismissed: UIViewController
+  ) -> UIViewControllerAnimatedTransitioning? {
+    return SlideOutAnimationController()
   }
 }
